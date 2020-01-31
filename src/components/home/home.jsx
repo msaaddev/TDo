451c8e2abae37cdec51137852893295db0420c6f
msaaddev <mrsaadirfan@gmail.com>
2020-01-31 22:02:25 +0500

    📦NEW: Home

---
+++ src/components/home/home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './reset.css';
+import './home.css';
+import home from '../../images/home.jpg';
+
+class Home extends Component {
+	state = {};
+	render() {
+		return (
+			<dir className="main_container">
+
+			</dir>
+		);
+	}
+}
+
+export default Home;
