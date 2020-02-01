f478211320b6549b961ae9f3c34b501b5922dc60
msaaddev <mrsaadirfan@gmail.com>
2020-02-01 16:26:40 +0500

    📦 NEW: media query

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import LoginButton from '../button/LoginButton';
 import './reset.css';
 import './home.css';
 
 class Home extends Component {
 	state = {
 		title: 'TDo - Everything you need',
-		description: 'A fast & easy way to keep track of your daily tasks'
+		description: 'A fast & easy way to keep track of your daily tasks',
+		button_text: "login"
 	};
 	render() {
 		return (
@@ -14,6 +16,7 @@ class Home extends Component {
 					<h1 id="welcome_title">
 						{this.state.title}
 						<p> {this.state.description}</p>
+						<LoginButton text={this.state.button_text}/>
 					</h1>
 				</div>
 			</div>
